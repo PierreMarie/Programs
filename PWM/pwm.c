15df9ea973cfdc1a8bd9f985496ff740eeff9838
PierreMarie <librantene@hotmail.fr>
2018-06-12 16:24:31 +0200

    Update pwm.c

--- PWM/pwm.c
+++ PWM/pwm.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include "wiringPi.h"
 
+// void pwmWrite (int pin, int value)		value = 0 - 1024
+// void pwmSetRange (unsigned int range)	range ? - 4096
+// void pwmSetClock (int divisor)		range ? - 4096		default 1024
+
+// PWM frequency = 19.2 MHz / ( pwmClock * pwmRange )
+// Min frequency = 1.14 Hz
+
+// void delayMicrosecondsHard (unsigned int howLong)
+// void delayMicroseconds (unsigned int howLong)
+
+
 int main (void)
 {
 	int bright;
